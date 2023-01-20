168cf971e72edc3ad718938bc8990c2ffd28d6d1
ethanDe <90716310+ethanDe@users.noreply.github.com>
2023-01-20 11:57:59 -0800

    Main functionality complete (1/20/2023)

--- assignment.py
+++ assignment.py
@@ -1,13 +1,10 @@
 #python
 #221 flask-post-requests
 
-import flask
-from flask import Flask
-import flask_cors
+from flask import Flask, request, render_template
 from flask_cors import CORS
-import time, json
-import sqlite3
 import random
+import time, json
 
 web = Flask(__name__)
 CORS(web)
@@ -19,7 +16,7 @@ def textfile():
         i +="\n"
         file.write(i)
 
-@web.route("/quote",methods=['POST','GET'])
+@web.route("/",methods=['POST','GET'])
 def main():
     #sample url: http://127.0.0.1:5000/
     #output: a text string literal, includes information for help
@@ -34,13 +31,20 @@ def admin():
     #sample url: http://127.0.0.1:5000/
     #output: a text string literal, includes information for help
     #response is not json encoded\
+    global file
     file = open('entry.txt', 'r')
-    Lines = file.readlines()
-
-    count = 0
-    for line in Lines:
-        count += 1
-        yield f"Line {count}: '{line.strip()}'</br>"
+    def working():
+        Lines = file.readlines()
+        count = 0
+        for line in Lines:
+            count += 1
+            if line == "":
+                line.truncate()
+            yield f"Quote {count}: '{line.strip()}'</br>"
+    working()
+    
+    file2 = "https://code.jquery.com/jquery-3.6.1.js"
+    file2 = file2.working()
 
 @web.route("/new",methods=['POST','GET'])
 def new():
@@ -56,7 +60,21 @@ def new():
         quote = [item.split('\n')[0] for item in quotes]
     return quote
 
-
+@web.route("/newquotes",methods=['POST','GET'])
+def newquote():
+    #sample url: http://127.0.0.1:5000/
+    #output: a text string literal, includes information for help
+    #response is not json encoded\
+    file = open("entry.txt", "r")
+    quotes = file.readlines()
+    new_data = request.form
+    new_data = dict(new_data)
+    if new_data['val1'] not in quotes:
+        with open('entry.txt', 'a') as i:
+            i.write(f"{new_data['val1']}\n")
+            return "addition a success"
+    else:
+        print("Already entered")
 
 textfile()
 web.run()
